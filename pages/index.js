7310a857daccb68783a6863b8f6a9b2d7159d7bf
Alexander-Kolotow <alexkolotow@gmail.com>
2024-02-20 16:53:13 +0100

    refactoring part 1

--- pages/index.js
+++ pages/index.js
@@ -1,144 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import useSWR from 'swr';
-import { mutate } from 'swr';
+import { Container, Header, StatsContainer, Stat, ResetButton, QuizCard, Question, Option, CheckAnswerButton, NavigationButton, Toast } from '../quizstyles/quizStyles';
+import useSWR, { mutate } from 'swr';
 import useLocalStorageState from 'use-local-storage-state';
 import Confetti from 'react-confetti';
-
-const Container = styled.div`
-  position: relative;
-  margin-top: 50px;
-  text-align: center;
-  max-width: 800px;
-  margin: auto;
-  background-color: #f8fbff;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.4);
-`;
-
-const ResetButton = styled.button`
-  padding: 5px 20px;
-  background-color: red;
-  color: white;
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
-  font-size: 20px;
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
-
-const StatsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-bottom: 20px;
-  padding: 15px;
-`;
-
-const Stat = styled.div`
-  margin: 0 10px;
-  font-size: 20px;
-  color: black;
-`;
-
-const CheckAnswerButton = styled.button`
-  padding: 10px 20px;
-  background-color: #4CAF50; 
-  color: white; 
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  transition: background-color 0.3s, transform 0.2s;
-  opacity: ${(props) => (props.isAnswered ? 0.5 : 1)};
-  pointer-events: ${(props) => (props.isanswered ? 'none' : 'auto')};
-
-  &:hover {
-    background-color: #43A047; 
-    transform: scale(1.05);
-  }
-
-  &:active {
-    transform: scale(0.95);
-  }
-`;
-const Header = styled.h1`
-  font-size: 32px;
-  color: #217aff; 
-  margin-bottom: 30px;
-`;
-
-const QuizCard = styled.div`
-  background-color: #ffffff; 
-  border-radius: 8px;
-  padding: 20px;
-  margin-bottom: 30px;
-  box-shadow: 0 0 15px rgba(0,0,0,0.05);
-`;
-
-const Question = styled.h2`
-  font-size: 25px;
-  color: #217aff; 
-  margin-bottom: 20px;
-`;
-
-const Option = styled.button`
-  display: inline-block;
-  margin: 10px;
-  padding: 10px 20px;
-  background-color: whitesmoke;
-  border: ${(props) => (props.selected && !props.isanswered ? '4px solid #006400' : '2px solid #217aff')};
-  border-radius: 20px;
-  cursor: pointer;
-  font-size: 16px;
-  transition: background-color 0.3s, transform 0.2s;
-  
-
-  &:hover {
-    border: 4px solid #006400;
-    transform: translateY(-5px);
-  }
-
-  &:disabled {
-    opacity: 0.5;
-    cursor: default;
-    pointer-events: ${(props) => (props.isanswered ? 'none' : 'auto')};
-  }
-`;
-
-const Toast = styled.div`
-  background-color: ${(props) => (props.type === 'correct' ? '#4CAF50' : '#F44336')};
-  color: white;
-  padding: 10px;
-  border-radius: 5px;
-  margin-bottom: 20px;
-  transition: opacity 0.5s ease-out;
-`;
-
-const NavigationButton = styled.button`
-  margin: 0 10px;
-  padding: 10px 20px;
-  background-color: #FFD700;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #CC9900;
-  }
-
-  &:disabled {
-    background-color: #cccccc;
-    opacity: 0.5;
-    cursor: default;
-  }
-`;
-
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
@@ -206,15 +70,12 @@ const HomePage = () => {
         setShowConfetti(false);
       }, 10000);
   
-      // Cleanup-Funktion, um den Timer zu löschen, falls die Komponente unmontiert wird
       return () => clearTimeout(timer);
     }
   }, [totalCount]); 
   
   if (error) return <div>Failed to load</div>;
-  if (!quizData) return <div>Loading...</div>; 
-
-  
+  if (!quizData) return <div>Loading...</div>;
 
   const handleOptionSelect = (option) => {
     if (!quizData[currentQuestion].answered) {
@@ -240,19 +101,19 @@ const HomePage = () => {
     }
     setTotalCount((totalCount) => totalCount + 1);
   
-    // Setzen des answered Zustands für die aktuelle Frage auf true, bevor die Netzwerkanfrage gesendet wird
+    
     const updatedQuizData = [...quizData];
     updatedQuizData[currentQuestion].answered = true;
-    // Aktualisieren des lokalen Zustands sofort, um die UI zu reflektieren
+   
     mutate(`/api/quizzes`, updatedQuizData, false); 
   
     try {
-      // Netzwerkanfrage, um den answered Zustand im Backend zu aktualisieren
+      
       await updateQuizStatus(quizData[currentQuestion]._id, true);
-      // Kein erneuter Aufruf von mutate() notwendig, da der Zustand bereits aktualisiert wurde
+      
     } catch (error) {
       console.error("Failed to update quiz status", error);
-      // Optional: Rückgängig machen der Zustandsänderung im Fehlerfall
+      
       updatedQuizData[currentQuestion].answered = false;
       mutate(`/api/quizzes`, updatedQuizData, false);
     }
@@ -264,7 +125,7 @@ const HomePage = () => {
   
 
 const handleResetQuiz = async () => {
-  const confirmReset = window.confirm('Are you sure you want to reset all Quiz Cards? Accordingly, your statistics will be reset to 0, and you will start the quiz from the beginning.');
+  const confirmReset = window.confirm('Are you sure you want to reset all Quiz Cards? Doing so will reset your statistics to 0, and you will start the quiz from the beginning.');
   if (confirmReset) {
 
     setCorrectCount(0);
@@ -311,7 +172,7 @@ const handleResetQuiz = async () => {
       <StatsContainer>
         <Stat>✅: {correctCount}</Stat>
         <Stat>❌: {wrongCount}</Stat>
-        <Stat>Total: {totalCount}</Stat>
+        <Stat>&sum;: {totalCount}</Stat>
       </StatsContainer>
       </Header>
 
